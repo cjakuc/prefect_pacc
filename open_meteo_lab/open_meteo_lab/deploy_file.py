fd267c474c161b0c81889301fcf2ede1449e779a
cjakuc <cjakuc@entelligent.com>
2023-02-16 09:27:42 -0500

    try to add github deployment via block

--- open_meteo_lab/open_meteo_lab/deploy_file.py
+++ open_meteo_lab/open_meteo_lab/deploy_file.py
@@ -1,13 +1,17 @@
 from open_meteo_lab.open_meteo import run_pipeline
 from prefect.deployments import Deployment
 from prefect.orion.schemas.schedules import RRuleSchedule, CronSchedule
+from prefect.filesystems import GitHub
+
+github_block = GitHub.load("github-access")
 
 rr = RRuleSchedule(rrule="FREQ=MINUTELY;INTERVAL=1;COUNT=4")
 deployment = Deployment.build_from_flow(
     flow=run_pipeline,
     name="RRule scheduled Python deployment",
     parameters={'coordinates':[(40.7128, 74.0060, 'NYC')]},
-    schedule=rr
+    schedule=rr,
+    storage=github_block
 )
 
 if __name__ == '__main__':
